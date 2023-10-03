d667a99dc72398d886ce6f191ec929eff358cacf
wikidips <wikidips18@gmail.com>
2023-10-03 22:29:19 +0100

    t

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -3,8 +3,15 @@ import random
 
 number = random.randint(-10000, 10000)
 last_digit = abs(number) % 10
-last_digit *= -1 if number < 0 else 1
 
-comparison = "greater than 5" if last_digit > 5 else "0" if last_digit == 0 else "less than 6 and not 0"
-print(f"Last digit of {number} is {last_digit} and is {comparison}")
+if number < 0:
+    last_digit = -last_digit
+
+print(f"Last digit of {number} is {last_digit} and is ", end="")
+if last_digit > 5:
+    print("greater than 5")
+elif last_digit == 0:
+    print("0")
+else:
+    print("less than 6 and not 0")
 
