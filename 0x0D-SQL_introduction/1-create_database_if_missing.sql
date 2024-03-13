292da72fdc65b8aeb96fcdad381905a2f25af160
Wikidips <wikidips18@gmail.com>
2024-03-13 00:15:16 +0000

    saop

--- 0x0D-SQL_introduction/1-create_database_if_missing.sql
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -1,4 +1,2 @@
---create a database hbtn_0c_0 if it doesn't exists
--- exit with exit code 0 if it already does
-CREATE DATABASE IF NOT EXISTS `hbtn_0c_0`;
-
+-- Ceates database if not exist
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
