244d97b75ed93c666b0fc2cea77cff74b20b4a93
Wikidips <wikidips18@gmail.com>
2024-04-10 00:46:15 +0000

    thinkiing

---
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+/*
+  Rectangle Class that init height and width
+*/
+
+module.exports = class Rectangle {
+  constructor (w, h) {
+    this.width = w;
+    this.height = h;
+  }
+};
