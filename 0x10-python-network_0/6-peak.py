3d93ad4ff5515052097b01377d4ab61ae735f952
Wikidips <wikidips18@gmail.com>
2024-04-26 02:02:24 +0100

    update executable file

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,26 +1,30 @@
 #!/usr/bin/python3
-""" find peak in a list """
+"""
+find the peak in an unordered list of
+integers
+"""
 
 
 def find_peak(list_of_integers):
-    """ Finds the peak in a list of integers """
-    if list_of_integers == []:
+    """
+    find the peak in a list of intergers
+    Args:
+        list_of_integers(list) -> List of intergers
+    Return:
+        int(the peak integer(s))
+    """
+    if len(list_of_integers) == 0:
         return None
+    if len(list_of_integers) == 1:
+        return list_of_integers[0]
+    if len(list_of_integers) == 2:
+        return max(list_of_integers)
 
-    length = len(list_of_integers)
-    m = int(length / 2)
-    li = list_of_integers
-
-    if m - 1 < 0 and m + 1 >= length:
-        return li[m]
-    elif m - 1 < 0:
-        return li[m] if li[m] > li[m + 1] else li[m + 1]
-    elif m + 1 >= length:
-        return li[m] if li[m] > li[m - 1] else li[m - 1]
-
-    if li[m - 1] < li[m] > li[m + 1]:
-        return li[m]
-
-    if li[m + 1] > li[m - 1]:
-        return find_peak(li[m:])
-    return find_peak(li[:m])
+    mid = int(len(list_of_integers) / 2)
+    peak = list_of_integers[mid]
+    if peak > list_of_integers[mid - 1] and peak > list_of_integers[mid + 1]:
+        return peak
+    elif peak < list_of_integers[mid - 1]:
+        return find_peak(list_of_integers[:mid])
+    else:
+        return find_peak(list_of_integers[mid + 1:])
